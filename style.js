180290f149606df77eb301af29be7a917935ec37
mariam mostafa <mariammostafa7799@gmail.com>
2020-03-08 15:50:12 +0200

    inittial commit

---
+++ style.js
@@ -0,0 +1,11 @@
+
+/*header */
+
+$(window).scroll(function() {
+    if ($(this).scrollTop() > 100) {
+      $('#header').addClass('header-scrolled');
+    } else {
+      $('#header').removeClass('header-scrolled');
+    }
+  });
+
